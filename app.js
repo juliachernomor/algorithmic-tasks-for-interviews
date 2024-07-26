27a21c2d131fafb3303e885f7dc394e6d1979aaf
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-07-26 17:45:12 +0300

    new ффылы

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ console.log(getPalindrome('sum summus mus'))
 // console.log(getSubstringA('ADGEIGHUIKAAAAAATT')) //IGHUIKAAAAAATT -14
 // // console.log(getSubstringA('HGJFSAAAJKASHJAAGEHAAAAAAAAAAAAAF'))//HGJFSAAAJKASHJAAG-17
 
-//задав строку верните символ, который чаще всего используется в строке
+/*//задав строку верните символ, который чаще всего используется в строке
 // // 1 вариант
 // function maxChar(str){
 //     let obj={};
@@ -158,25 +158,177 @@ console.log(getPalindrome('sum summus mus'))
 // return maxLetter
 // }
 
+// // 2 вариант
+// function maxChar(str){
+//     let obj={};
+//     let max=0;
+//     let maxLetter='';
+//     for(let char of str) {
+//         obj[char]=obj[char]+1||1; // if(obj[element]){obj[element] = obj[element] + 1;} else {obj[element] = 1;}
+//     };
+// for(let key in obj){
+//     if(obj[key]>max) {
+//         max= obj[key];
+//         maxLetter=key
+//     }
+// }
+// return maxLetter
+// }
+// console.log(maxChar('abccccccccd'))
+// console.log(maxChar('abc1111ccчовочоллаовгыкыкыааааааааааааааиьььььььььььььььььььььььььььь'))
+
+// 3 вариант
 function maxChar(str){
     let obj={};
     let max=0;
     let maxLetter='';
     for(let char of str) {
-        obj[char]=obj[char]+1||1;
+        obj[char]=obj[char]+1||1; // if(obj[element]){obj[element] = obj[element] + 1;} else {obj[element] = 1;}
     };
-for(let key in obj){
-    if(obj[key]>max) {
-        max= obj[key];
-        maxLetter=key
+for(const  [key, value] of Object.entries(obj)){//Object.entries(obj) - массив [ключ, значение],[ключ, значение],[ключ, значение]
+    if(value>max) {
+        max = value;
+        maxLetter=key;
     }
 }
 return maxLetter
 }
-
+console.log("===")//true
 console.log(maxChar('abccccccccd'))
 console.log(maxChar('abc1111ccчовочоллаовгыкыкыааааааааааааааиьььььььььььььььььььььььььььь'))
+*/
 
-//какие символы в строке встречаются чаще всего
-//содержит ли строка a те же символы, что и строка В (грязеводолечебница - водогрязелечебница, бокал - колба(анаграммы))
-//есть ли в данной строке какие-либо повторяющиеся символы
+
+
+
+/*//содержит ли строка a те же символы, что и строка В (грязеводолечебница - водогрязелечебница, бокал - колба(анаграммы)
+//анаграммы
+function anagr(str1,str2) {
+    let obj1={};
+    let obj2={};
+    for(let letter of str1){
+        obj1[letter]=obj1[letter]+1||1;
+    }
+    for(let letter of str2){
+        obj2[letter]=obj2[letter]+1||1;
+    }
+    if (Object.keys(obj1).length !== Object.keys(obj1).length) {
+        return false;
+    }
+    for (let char in obj1) {
+        if(obj1[char] !== obj2[char] ) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(anagr('ласка','скала'))
+console.log(anagr('нос','лоб'))
+console.log(anagr('ласка','сскала'))
+*/
+
+
+
+
+/*//есть ли в данной строке повторяющиеся символы? если да, то true
+function moreThanOne(str1) {
+    let obj ={};
+    for (let letter of str1){
+        obj[letter]=obj[letter]+1||1;
+    }
+    for (let key in obj) {
+        if (obj[key] >1){
+            return true;
+        }
+        return false;
+    }
+}
+console.log("===")//true
+console.log(moreThanOne("hhfghjljljjljkl"))//true
+console.log(moreThanOne("asdfghjkl"))//false
+*/
+
+/*// массив делить на подмассивы  [1,2,3,4,5,6,7,8,9,0,65,43]-->[ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 0, 65, 43 ] ]
+// 1способ (мое решение)
+function subMass(mass, number) {
+    let initialMass=mass.slice();
+    let sub=[];
+    for(let i=0; i<initialMass.length/number;i++){
+    sub.push(mass.splice(0,number));
+    }
+    return sub;
+}
+console.log(subMass([1,2,3,4,5,6,7,8,9,0,65,43], 3))
+*/
+
+
+/*// 2вариант
+function subMass(mass, number) {
+    let sub=[];
+    let index=0;
+    for(let i=0; i<mass.length/number;i++){
+        sub.push(mass.slice(index,index+number));
+        index+=number
+    }
+    return sub;
+}
+
+console.log(subMass([1,2,3,4,5,6,7,8,9,0,65,43], 3))*/
+
+/*// 3вариант
+function subMass(mass, number) {
+    let sub=[];
+    let index=0;
+    while (index<mass.length){
+        sub.push(mass.slice(index,index+number));
+        index+=number
+    }
+    return sub;
+}
+
+console.log(subMass([1,2,3,4,5,6,7,8,9,0,65,43], 5));*/
+
+/*// capitalize изменить первую букву а и б сидели на трубе, а упала-->А И Б Сидели На Трубе, А Упала
+function capitalize(str){
+    let mass = str.split(" ");
+    let result=[];
+for (let letter of mass) {
+    result.push(letter[0].toUpperCase() + letter.slice(1));
+}
+return result.join(" ");
+}
+
+console.log(capitalize("а и б сидели на трубе, а упала"))*/
+
+/*// capitalize изменить первую букву а и б сидели на трубе, а упала-->А И Б Сидели На Трубе, А Упала
+function capitalize(str){
+    return str.split(" ").map(letter=>letter[0].toUpperCase() + letter.slice(1)).join(" ");
+}
+
+console.log(capitalize("а и б сидели на трубе, а упала"))
+*/
+
+function anagram2(str1, str2) {
+    let obj1={};
+    let obj2={};
+    const first=str1.toLowerCase().replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    const second=str2.toLowerCase().replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+
+    for( let letter of first) {
+        obj1[letter]=obj1[letter]+1||1
+    }
+    for( let letter of second) {
+        obj2[letter]=obj2[letter]+1||1
+    }
+
+    if(Object.keys(obj1).length !== Object.keys(obj2).length) {return false}
+    for(let key in obj1) {
+        if(obj1[key] !== obj2[key]){
+            return false;
+        }
+    }
+    return true
+    }
+    console.log("===")
+    console.log(anagram2("coding money", "money coding"))
+    console.log(anagram2("RAIL! SAFETY!", "fairy tales"))
